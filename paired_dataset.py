0d42cb28b752c6719441d2bad76b3dfa7a20f5e5
jaramrammm <jaramrammm@gmail.com>
2025-05-27 23:05:34 +0900

    make code for paired_dataset

---
+++ paired_dataset.py
@@ -0,0 +1,46 @@
+import torch
+from torch.utils.data import Dataset
+
+
+class PersonaDataset(Dataset):
+    def __init__(self, data, tokenizer, persona_list, max_seq_len=50):
+        self.data = data  # pd.DataFrame
+        self.tokenizer = tokenizer
+        self.persona_list = persona_list
+        self.max_seq_len = max_seq_len
+
+        self.bos = tokenizer.bos_token_id or tokenizer.cls_token_id
+        self.eos = tokenizer.eos_token_id or tokenizer.sep_token_id
+        self.pad = tokenizer.pad_token_id
+        self.ignore_index = -100
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def padding_ids(self, seq, pad_id):
+        if len(seq) > self.max_seq_len:
+            return seq[:self.max_seq_len]
+        return seq + [pad_id] * (self.max_seq_len - len(seq))
+
+    def __getitem__(self, i):
+        row = self.data.iloc[i]
+        persona_tag = f"<{row['label']}>"
+        
+        # 2. source sentence
+        source = f"{persona_tag} {row['source']}"
+        source_ids = [self.bos] + self.tokenizer(source, add_special_tokens=False)["input_ids"] + [self.eos]
+        source_attention_mask = [1] * len(source_ids)
+
+        # 3. target sentence
+        target = row['target']
+        target_ids = self.tokenizer(target, add_special_tokens=False)["input_ids"] + [self.eos]
+
+        input_ids = self.padding_ids(source_ids, self.pad)
+        attention_mask = self.padding_ids(source_attention_mask, 0)
+        labels = self.padding_ids(target_ids, self.ignore_index)
+
+        return {
+            "input_ids": torch.tensor(input_ids, dtype=torch.long),
+            "attention_mask": torch.tensor(attention_mask, dtype=torch.float),
+            "labels": torch.tensor(labels, dtype=torch.long)
+        }
---
+++ paired_train.py
@@ -0,0 +1,56 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, Seq2SeqTrainer, Seq2SeqTrainingArguments, DataCollatorForSeq2Seq
+from paired_dataset import PersonaDataset
+import torch
+from utils import seed_everything
+
+
+seed_everything(42)
+model_name = "cosmoquester/bart-ko-base"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+
+df = pd.read_csv("paired_dataset.csv", encoding='utf-8')
+
+persona_list = ['android', 'azae', 'chat', 'choding', 'emoticon', 'enfp',
+                'gentle', 'halbae', 'halmae', 'joongding', 'king', 'naruto',
+                'seonbi', 'sosim', 'translator']
+
+
+print(len(df))
+train_df, val_df = train_test_split(df, test_size=0.1, stratify=df['label'], random_state=42)
+print(len(train_df))
+print(len(val_df))
+train_dataset = PersonaDataset(train_df, tokenizer, persona_list)
+val_dataset = PersonaDataset(val_df, tokenizer, persona_list)
+
+data_collator = DataCollatorForSeq2Seq(tokenizer, model=model)
+
+device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+model.to(device)
+
+training_args = Seq2SeqTrainingArguments(
+    output_dir="finetune",
+    per_device_train_batch_size=16,
+    learning_rate=5e-5,
+    num_train_epochs=3,
+    logging_steps=1,
+    save_steps=200,
+    save_total_limit=5,
+    eval_strategy="steps",
+    eval_steps=200,
+    report_to="wandb",
+    fp16=False
+)
+
+trainer = Seq2SeqTrainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_dataset,
+    eval_dataset=val_dataset,
+    tokenizer=tokenizer,
+    data_collator=data_collator,
+)
+
+trainer.train()
